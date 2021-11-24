27d5f978752efaff75cb1880048e47db9ccc795c
paulovictorpv25 <paulovictornlds@hotmail.com.br>
2021-11-24 14:51:29 -0300

    projeto exemplo spring com banco de dados

---
+++ src/main/java/com/example/accessingdata/UsuarioRepository.java
@@ -0,0 +1,8 @@
+package com.example.accessingdata;
+
+import org.springframework.data.repository.CrudRepository;
+
+
+public interface UsuarioRepository extends CrudRepository<Usuario, Integer> {
+
+}
